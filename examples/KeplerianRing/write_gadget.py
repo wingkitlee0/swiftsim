4c4e315d8f693afa3528398475533a2174cccfcc
Josh Borrow <joshua.borrow@durham.ac.uk>
2017-08-24 11:06:03 +0100

    updated gadget writing routunes to include units, etc. in separate functions.

--- examples/KeplerianRing/write_gadget.py
+++ examples/KeplerianRing/write_gadget.py
@@ -18,127 +18,290 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+# -----------------------------------------------------------------------------
+#
 # This program is a set of helper functions that write particle data to a
-# GADGET .hdf5 file *handle*.
+# GADGET .hdf5 file *handle*. It should also function as a piece of
+# documentation on the required attributes for SWIFT to function when running
+# in GADGET compatibility mode.
+# 
+# Example Usage:
+#
+# import write_gadget as wg
+# import h5py as h5
+$ import numpy as np
+#
+#
+# N_PARTICLES = 1000
+#
+#
+# with h5.File("test.hdf5", "w") as f:
+#    wg.write_header(
+#        f,
+#        boxsize=100.,
+#        flag_entropy=1,
+#        np_total=[N_PARTICLES, 0, 0, 0, 0, 0],
+#        np_total_hw=[0, 0, 0, 0, 0, 0]
+#    )
+#
+#    wg.write_runtime_params(
+#        f,
+#        periodic_boundary=1,
+#    )
+#
+#    wg.write_units(
+#        f,
+#        current=1.,
+#        length=1.,
+#        mass=1.,
+#        temperature=1.,
+#        time=1.
+#    )
+#
+#    wg.write_block(
+#       f,
+#       part_type=0,
+#       pos=np.array([np.arange(N_PARTICLES)] * 3).T,
+#       vel=np.array([np.zeros(N_PARTICLES)] * 3).T,
+#       ids=np.arange(N_PARTICLES),
+#       mass=np.ones(N_PARTICLES,
+#       int_energy=np.zeros(N_PARTICLES),
+#       smoothing=np.ones(NP_ARTICLES),
 #
 ###############################################################################
 """
 
-def write_block(f, part_type, pos, vel, id, m=False, U=False, rho=False, hsml=False, pot=False, acc=False, dAdt=False, dt=False):
+
+def write_header(f, boxsize, flag_entropy, np_total, np_total_hw, other=False):
+    """ Writes the "Header" section of the hdf5 file. The parameters in this
+        function that are required are the ones that are required for SWIFT
+        to function; note that the MassTable is **ignored** and that all
+        particle masses should be placed into the particle data arrays.
+
+        @param: f | file handle
+            - the file handle of the hdf5 object (use h5py.File(filename, "w")
+              to open a file handle of the correct type).
+
+        @param boxsize | float / list (2D / 3D)
+            - the boxsize. If a float is given it is assumed that the box is
+              the same size in all directions.
+
+        @param flag_entropy | int (0/1)
+            - sets Flag_Entropy_ICs. This is a historical variable for cross
+              compatibility with Gadget-3
+
+        @param np_total | list (6D)
+            - the total number of particles required of each type.
+
+                Type/Index | Symbolic Type Name
+               ------------|--------------------
+                    0      |       Gas
+                    1      |       Halo
+                    2      |       Disk
+                    3      |       Bulge
+                    4      |       Stars
+                    5      |       Bndry
+
+        @param np_total_hw | list (6D)
+            - the number of high-word particles in the file.
+
+
+        @param other | dictionary | optional
+            - a dictionary with any other parameters that you wish to pass into
+              the file header. They will be passed such that the key is the
+              name of the attribute in the hdf5 file.
+
     """
-    Write a block of data (i.e. a set of particle types).
 
-    @param: f | file handle
-        - the hdf5 file handle to write to
+    # We'll first build a dictionary to iterate through.
 
-    @param: part_type | int
-        - the GADGET particle type number. See the GADGET documentation.
+    default_attributes = {
+        "Boxsize" : boxsize,
+        "Flag_Entropy_ICs" : flag_entropy,
+        "NumPart_Total" : np_total,
+        "NumPart_Total_HighWord" : np_total_hw,
+    }
 
-    @param: pos | array-like
-        - the array of particle positions with shape (n_particles, 3)
-
-    @param: vel | array-like
-        - the array of particle velocities with shape (n_particles, 3)
-
-    @param: id | array-like
-        - array of particle ids with shape (n_particles, 3)
-
-    @param: m | array-like | optional
-        - array of particle masses (only useful if particle masses change with
-          time, otherwise just set the mass in the header).
-
-    @param: U | array-like | optional
-        - array of the internal energies associated with the particles.
-
-    @param: rho | array-like | optional
-        - array of the densities of the particles.
-
-    @param: hsml | array-like | optional
-        - array of the smoothing lengths of the particles (again, this can be
-          set for all particles elsewhere).
-    
-    ...
-    """
-    part_type = f.create_group("PartType{}".format(part_type))
-    n_particles = len(id)
-
-    # Create datasets
-    positions = part_type.create_dataset("Coordinates", (n_particles, 3), data=pos)
-    velocities = part_type.create_dataset("Velocities", (n_particles, 3))
-    ids = part_type.create_dataset("ParticleIDs", (n_particles,))
-
-    # Oh gosh this is very wasteful code but it *works*.
-    if m: #.any()
-        masses = type.create_dataset("Masses", (n_particles, ))
-    if U: #.any()
-        internal_energies = type.create_dataset("InternalEnergy", (n_particles, ))
-    if rho: #.any()
-        density = type.create_dataset("Density", (n_particles, ))
-    if hsml: #.any()
-        smoothing_length = type.create_dataset("SmoothingLength", (n_particles, ))
-    if pot: #.any()
-        potential = type.create_dataset("Potential", (n_particles, ))
-    if acc: #.any()
-        accelerations = type.create_dataset("Acceleration", (n_particles, 3))
-    if dAdt: #.any()
-        rate_of_change_of_entropy = type.create_dataset("RateOfChangeOfEntropy", (n_particles, ))
-    if dt: #.any()
-        timestep = type.create_dataset("TimeStep", (n_particles, ))
-
-    # Now fill
-
-    velocities[...] = vel
-    ids[...] = id
-
-    if m: #.any()
-        masses[...] = m
-    if U: #.any()
-        internal_energies[...] = U
-    if rho: #.any()
-        density[...] = rho
-    if hsml: #.any()
-        smoothing_length[...] = hsml
-    if pot: #.any()
-        potential[...] = pot
-    if acc: #.any()
-        accelerations[...] = acc
-    if dAdt: #.any()
-        rate_of_change_of_entropy[...] = dAdt
-    if dt: #.any()
-        timestep[...] = dt
-
-    return f
-
-
-def write_head(f, npart, massarr, time, z=False, flagsfr=False, flagfeedback=False, nall=False, flagcooling=False, numfiles=1, omega0=0.3, omegalambda=0.7, hubbleparam=0.7, flagage=False, flagmetals=False, nallhw=0, flagentrics=False):
-    header = f.create_group('Header')
-
-    header.attrs['NumPart_ThisFile'] = npart
-    header.attrs['MassTable'] = massarr
-    header.attrs['Time'] = time
-    header.attrs['NumFilesPerSnapshot'] = numfiles
-    header.attrs['Omega0'] = omega0
-    header.attrs['OmegaLambda'] = omegalambda
-    header.attrs['HubbleParam'] = hubbleparam
-    header.attrs['NumPart_Total_HW'] = nallhw
-
-    if z: #.any()
-        header.attrs['Redshift'] = z
-    if flagsfr: #.any()
-        header.attrs['Flag_Sfr'] = flagsfr
-    if flagfeedback: #.any()
-        header.attrs['Flag_Feedback'] = flagfeedback
-    if nall: #.any()
-        header.attrs['NumPart_Total'] = nall
+    if other:
+        attributes = dict(default_attributes, **other)
     else:
-        header.attrs['NumPart_Total'] = npart
-    if flagcooling: #.any()
-        header.attrs['Flag_Cooling'] = flagcooling
-    if flagage: #.any()
-        header.attrs['Flag_StellarAge'] = flagage
-    if flagmetals: #.any()
-        header.attrs['Flag_Metals'] = flagmetals
-    if flagentrics: #.any()
-        header.attrs['Flag_Entropy_ICs'] = flagentrics
+        attributes = default_attributes
 
-    return f
+    header = f.create_group("Header")
+
+    # For some reason there isn't a direct dictionary interface (at least one
+    # that is documented, so we are stuck doing this loop...
+
+    for name, value in attributes.items():
+        header[name] = value
+
+    return
+
+
+def write_runtime_params(f, periodic_boundary, other=False):
+    """ Writes the "RuntimeParams" section in the hdf5 file. The parameters in
+        this function that are required are also required for SWIFT to function.
+        If you wish to pass extra arguments into the runtime parameters you
+        may do that by providing a dictionary to other.
+
+        @param: f | file handle
+            - the file handle of the hdf5 object (use h5py.File(filename, "w")
+              to open a file handle of the correct type).
+
+        @param: periodic_boundary | int (0/1)
+            - the condition for periodic boundary conditions -- they are 'on'
+              if this variable is 1, and off if it is 0. Note that SWIFT
+              currently requires periodic boundary conditions to run (as of
+              August 2017).
+
+        @param other | dictionary | optional
+            - a dictionary with any other parameters that you wish to pass into
+              the RuntimeParams. They will be passed such that the key is the
+              name of the attribute in the hdf5 file.
+    """
+
+    # First build the dictionary
+
+    default_attributes = {
+        "PeriodicBoundaryConditions" : periodic_boundary,
+    }
+
+    if other:
+        attributes = dict(default_attributes, **other)
+    else:
+        attributes = default_attributes
+
+    runtime = f.create_group("RuntimeParams")
+
+    for name, value in attributes.items():
+        runtime[name] = value
+
+    return
+
+
+def write_units(f, current, length, mass, temperature, time, other=False):
+    """ Writes the "RuntimeParams" section in the hdf5 file. The parameters in
+        this function that are required are also required for SWIFT to function.
+        If you wish to pass extra arguments into the runtime parameters you
+        may do that by providing a dictionary to other.
+
+        @param: f | file handle
+            - the file handle of the hdf5 object (use h5py.File(filename, "w")
+              to open a file handle of the correct type).
+
+        @param: current | float
+            - the current conversion factor in cgs units.
+
+        @param: length | float
+            - the length conversion factor in cgs units.
+
+        @param: mass | float
+            - the mass conversion factor in cgs units.
+
+        @param: temperature | float
+            - the temperature conversion factor in cgs units.
+
+        @param: time | float
+            - the time conversion factor in cgs units.
+
+        @param: other | dictionary | optional
+            - a dictionary with any other parameters that you wish to pass into
+              the units attributes. They will be passed such that the key is
+              the name of the attribute in the hdf5 file.
+    """
+
+    # First build the dictionary
+
+    default_attributes = {
+        "Unit current in cgs (U_I)": current,
+        "Unit length in cgs (U_L)": length,
+        "Unit mass in cgs (U_M)": mass,
+        "Unit temperature in cgs (U_T)": temperature,
+        "Unit time in cgs (U_t)": time,
+    }
+
+    if other:
+        attributes = dict(default_attributes, **other)
+    else:
+        attributes = default_attributes
+
+    units = f.create_group("Units")
+
+    for name, value in attributes:
+        units[name] = value
+
+    return
+
+
+def write_block(f, part_type, pos, vel, ids, mass, int_energy, smoothing, other=False):
+    """ Writes a given block of data to PartType{part_type}. The above
+        required parameters are required for SWIFT to run.
+
+        @param: f | file handle
+            - the file handle of the hdf5 object.
+
+        @param part_type | int (0-5):
+            - the identifiying number of the particle type.
+
+                Type/Index | Symbolic Type Name
+               ------------|--------------------
+                    0      |       Gas
+                    1      |       Halo
+                    2      |       Disk
+                    3      |       Bulge
+                    4      |       Stars
+                    5      |       Bndry
+
+        @param pos | numpy.array
+            - the array of particle positions with shape (n_particles, 3).
+
+        @param vel | numpy.array
+            - the array of particle velocities with shape (n_particles, 3).
+
+        @param ids | numpy.array
+            - the ids of the particles. Please note that particle IDs in
+              SWIFT must be strictly positive. Shape (n_particles, 1)
+
+        @param mass | numpy.array
+            - the masses of the particles. In SWIFT MassTable in the header
+              is ignored and so particle masses must be provided here. Shape
+              (n_particles, 1)
+
+        @param int_energy | numpy.array
+            - the internal energies of the particles. Shape (n_particles, 1)
+
+        @param smoothing | numpy.array
+            - the smoothing lenghts of the individual particles. Please note 
+              that this cannot be supplied only in the parameterfile and must
+              be provided on a particle-by-particle basis in SWIFT. Shape
+              (n_particles, 1)
+
+        @param: other | dictionary | optional
+            - a dictionary with any other parameters that you wish to pass into
+              the particle data. They will be passed such that the key is
+              the name of the dataset in the hdf5 file.
+    """
+    
+    # Build the dictionary
+
+    default_data = {
+        "Coordinates" : pos,
+        "Velocities" : vel,
+        "ParticleIDs" : ids,
+        "Masses" : mass,
+        "InternalEnergy" : int_energy,
+        "SmoothingLength" : smoothing,
+    }
+
+    if other:
+        data = dict(default_data, **other)
+    else:
+        data = default_data
+
+    particles = f.create_group(f"PartType{part_type}")
+
+    for name, value in data:
+        particles.create_dataset(name, data=value)
+
+    return
+
