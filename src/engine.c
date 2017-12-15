a0306ad711294c33a78908cf8fad576445dcc196
lhausamm <loic_hausammann@hotmail.com>
2017-12-15 18:16:32 +0100

    Move write_file_header after setting some variables

--- src/engine.c
+++ src/engine.c
@@ -5924,11 +5924,6 @@ void engine_config(int restart, struct engine *e, struct swift_params *params,
           e->time_first_statistics, e->time_begin);
   }
 
-#if defined(WITH_LOGGER)
-  if (e->nodeID == 0)
-    message("Expected output of over 9000\n Should write a real message...");
-#endif
-
   /* Find the time of the first output */
   engine_compute_next_snapshot_time(e);
 
@@ -6071,7 +6066,15 @@ void engine_config(int restart, struct engine *e, struct swift_params *params,
     }
   }
 
-/* Free the affinity stuff */
+#ifdef WITH_LOGGER
+  if (e->nodeID == 0)
+    message("Expected output of over 9000\n Should write a real message...");
+  logger_write_file_header(dump_file, e);
+  dump_ensure(dump_file, e->logger_size);
+  e->logger_time_offset = 0;
+#endif
+
+  /* Free the affinity stuff */
 #if defined(HAVE_SETAFFINITY)
   if (with_aff) {
     free(cpuid);
