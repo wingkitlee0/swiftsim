04a168641cb0a649dac29f062d72c540f72aeb59
lhausamm <loic_hausammann@hotmail.com>
2018-02-20 07:47:10 +0100

    Log all particles at the end

--- examples/main.c
+++ examples/main.c
@@ -1107,6 +1107,8 @@ int main(int argc, char *argv[]) {
   engine_print_stats(&e);
 #ifdef WITH_LOGGER
   engine_dump_index(&e);
+  dump_ensure(e.logger_dump, e.logger_size);
+  logger_log_all(parts, e.total_nr_parts, e.logger_dump);
   strcat(e.snapshotBaseName, "_end");
 #endif
   // write a final snapshot with logger, in order to facilitate a restart
